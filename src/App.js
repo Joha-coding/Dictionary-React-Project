82c1709de3af91a9975c96e97fcbbf3acc9b8af9
Joha <jotz@outlook.fr>
2024-01-17 12:06:27 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -4,14 +4,41 @@ import Garlic_Branding from "./Garlic_Branding.png";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={Garlic_Branding}
-          alt="Garlic logo"
-          className="App-logo img-fluid"
-        />
-        <button className="btn btn-primary shadow">Test Bootstrap</button>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={Garlic_Branding}
+            alt="Garlic logo"
+            className="App-logo img-fluid"
+          />
+        </header>
+        <footer className="text-center">
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Joha-coding"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Joha
+          </a>
+          , is open-sourced on{" "}
+          <a
+            href="https://github.com/Joha-coding/New-York-Project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://mellow-pie-5a1106.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
