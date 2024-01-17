f83343d9dd6a983f55c1fe15fd37dc68dd118a25
Joha <jotz@outlook.fr>
2024-01-17 11:04:58 +0100

    Initial Commit : Created initial structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Garlic_Branding from "./Garlic_Branding.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={Garlic_Branding}
+          alt="Garlic logo"
+          className="App-logo img-fluid"
+        />
+      </header>
+    </div>
+  );
+}
