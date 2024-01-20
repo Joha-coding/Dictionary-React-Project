cf47c7b37757e086e81735d67c412f3588f283b1
Joha <jotz@outlook.fr>
2024-01-20 14:09:12 +0100

    Inserted Pexels API to show pictures whenever a word is searched

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,32 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Results.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDicoResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDicoResponse);
+
+    let pexelApiKey =
+      "RA3eAXdc0238evxhXy3lwGb1oIRTZ7i9bQ6YzlQUzEKmmnj36Rmn90LC";
+    let pexelApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let headers = { Authorization: `${pexelApiKey}` };
+    axios.get(pexelApiUrl, { headers: headers }).then(handlePexelResponse);
   }
 
   function handleSubmit(event) {
@@ -44,6 +56,7 @@ export default function Dictionary(props) {
             <em>Suggestions : garlic, sunset, plant...</em>
           </div>
         </section>
+        <Photos photos={photos} />
         <Results results={results} />
       </div>
     );
